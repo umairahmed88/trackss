f91756122fba13c693f0ef9c4e248e14370d625b
umairahmed88 <umairahmed_88@ymail.com>
2021-08-29 14:14:50 +0500

    Seting up a new project

---
+++ src/screens/SignInScreen.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View, StyleSheet, Text } from 'react-native';
+
+const SigninScreen = () => {
+    return <Text>Sign In Screen</Text>
+};
+
+const styles = StyleSheet.create({});
+
+export default SigninScreen;
---
+++ src/screens/SignUpScreen.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View, StyleSheet, Text } from 'react-native';
+
+const SignupScreen = () => {
+    return <Text>Sign Up Screen</Text>
+};
+
+const styles = StyleSheet.create({});
+
+export default SignupScreen;
